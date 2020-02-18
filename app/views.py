a165a3cbacd374de5cb36709ef18d5949b308d19
Romaro Scott <scott.r.ja@gmail.com>
2020-02-17 21:05:42 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -22,7 +22,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Lateefah Smellie")
+    return render_template('about.html', name="Mario Scott")
 
 
 ###
